c7e56a8a62ea234bcb775d3a80d2aa6bf973b105
BullChallenger <am23am45@naver.com>
2023-04-08 16:28:31 +0900

    [FEAT] yml 파일 설정
    제목과 동일

---
+++ src/main/java/com/jwt/prac/jwt_with_security/JwtWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.prac.jwt_with_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/prac/jwt_with_security/JwtWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.prac.jwt_with_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
